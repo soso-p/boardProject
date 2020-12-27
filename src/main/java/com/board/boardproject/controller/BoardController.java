c54be98dfc866d63f651c57a96f3fedb56b77d0d
soso-p <parksohee113@gmail.com>
2020-12-27 16:19:11 +0900

    add request param

--- src/main/java/com/board/boardproject/controller/BoardController.java
+++ src/main/java/com/board/boardproject/controller/BoardController.java
@@ -1,12 +1,21 @@
 package com.board.boardproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
     @GetMapping("board")
-    public String board() {
+    public String board(@RequestParam(value = "user", required = false) String user, Model model) {
+        model.addAttribute("user", user);
         return "board";
+    }
+
+    @GetMapping("")
+    public String index(@RequestParam(value = "user", required = false) String user, Model model) {
+        model.addAttribute("user", user);
+        return "index";
     }
 }
